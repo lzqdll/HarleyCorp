29da7e6d866e83bc2b0db6cf4d7b8081306c932b
lzqdll <zhiliu@gorbel.com.cn>
2017-06-22 21:27:49 +0800

    Update Env.java

--- src/com/alibaba/dingtalk/openapi/demo/Env.java
+++ src/com/alibaba/dingtalk/openapi/demo/Env.java
@@ -6,10 +6,10 @@ public class Env {
 	
 	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
 	public static final String OA_BACKGROUND_URL = ""; 
-	public static final String CORP_ID = "ding28029cf6368a0723";
+	public static final String CORP_ID = "";
 	
-	public static final String CORP_SECRET = "f_w7lWlR391E08xkuZh7DoLHuud4YKHzj9cmJrP4DIosgrM55Lwh-Rdv1rJ9jLxe";
-	public static final String SSO_Secret = "IDTjkwXT8wvXu2jExFM3_L3DvUO9f48v4p-JUNUVT1S8xVVD7pQ2X-UxGZ80YaNu";
+	public static final String CORP_SECRET = "";
+	public static final String SSO_Secret = "";
 
 	public static String suiteTicket; 
 	public static String authCode; 
