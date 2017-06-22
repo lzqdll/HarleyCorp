ed839d8b1768e602a106d83b197afd02212bd158
zhiliu <zhiliu@L-ZHILIUX.GorbelTianjin.Local>
2017-06-22 10:56:08 +0800

    1st

---
+++ src/com/alibaba/dingtalk/openapi/demo/Env.java
@@ -0,0 +1,24 @@
+package com.alibaba.dingtalk.openapi.demo;
+
+
+
+public class Env {
+	
+	public static final String OAPI_HOST = "https://oapi.dingtalk.com";
+	public static final String OA_BACKGROUND_URL = ""; 
+	public static final String CORP_ID = "ding28029cf6368a0723";
+	
+	public static final String CORP_SECRET = "f_w7lWlR391E08xkuZh7DoLHuud4YKHzj9cmJrP4DIosgrM55Lwh-Rdv1rJ9jLxe";
+	public static final String SSO_Secret = "IDTjkwXT8wvXu2jExFM3_L3DvUO9f48v4p-JUNUVT1S8xVVD7pQ2X-UxGZ80YaNu";
+
+	public static String suiteTicket; 
+	public static String authCode; 
+	public static String suiteToken; 
+
+	public static final String CREATE_SUITE_KEY = "suite4xxxxxxxxxxxxxxx";
+	public static final String SUITE_KEY = "";
+	public static final String SUITE_SECRET = "";
+	public static final String TOKEN = "";
+	public static final String ENCODING_AES_KEY = "";
+	
+}
---
+++ src/com/alibaba/dingtalk/openapi/servlet/UserInfoServlet.java
@@ -0,0 +1,71 @@
+package com.alibaba.dingtalk.openapi.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.dingtalk.openapi.demo.OApiException;
+import com.alibaba.dingtalk.openapi.demo.auth.AuthHelper;
+import com.alibaba.dingtalk.openapi.demo.department.DepartmentHelper;
+import com.alibaba.dingtalk.openapi.demo.user.User;
+import com.alibaba.dingtalk.openapi.demo.user.UserHelper;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.dingtalk.open.client.api.model.corp.CorpUserDetail;
+
+/**
+ * Servlet implementation class userinfo
+ * 这个servlet用来获取用户信息
+ */
+public class UserInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserInfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @throws IOException 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+		// TODO Auto-generated method stub
+		String code = request.getParameter("code");
+		String corpId = request.getParameter("corpid");
+		System.out.println("code:"+code+" corpid:"+corpId);
+
+		try {
+			response.setContentType("text/html; charset=utf-8"); 
+
+			String accessToken = AuthHelper.getAccessToken();
+			System.out.println("access token:"+accessToken);
+			CorpUserDetail user = UserHelper.getUser(accessToken, UserHelper.getUserInfo(accessToken, code).getUserid());
+			String userJson = JSON.toJSONString(user);
+			response.getWriter().append(userJson);
+			System.out.println("userjson:"+userJson);
+			
+		} catch ( Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response.getWriter().append(e.getMessage());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
